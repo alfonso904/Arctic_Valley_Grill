fe11e4b6bc5261d2be1bd72855e791ac2dac5301
alfonso904 <74293312+alfonso904@users.noreply.github.com>
2020-12-16 07:34:49 -0900

    x

--- dist/Arctic.dev.js
+++ dist/Arctic.dev.js
@@ -1,14 +1,11 @@
 "use strict";
 
-// When the user scrolls the page, execute myFunction
 window.onscroll = function () {
   myFunction();
-}; // Get the navbar
+};
 
-
-var navbar = document.getElementById("navbar"); // Get the offset position of the navbar
-
-var sticky = navbar.offsetTop; // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
